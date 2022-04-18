238d4da823bb6549a02aa806992396bcb3696594
William Cadiz <wcadz27@gmail.com>
2022-04-18 14:25:34 -0700

    reuploaded image files to IPFS to ensure app to be distributed and avoid any problems when centralized image file systems shut down such as imgur

--- scripts/run.js
+++ scripts/run.js
@@ -3,9 +3,9 @@ const main = async () => {
   const gameContract = await gameContractFactory.deploy(
     ["Tanjiro", "Zenitsu", "Inosuke"],
     [
-      "https://cdn.technadu.com/wp-content/uploads/2022/02/Tanjiro-x-Demon-Slayer-mark.jpg",
-      "https://static3.cbrimages.com/wordpress/wp-content/uploads/2020/02/demon-slayer-zenitsu-thunderclap-and-flash.jpg?q=50&fit=crop&w=740&h=370&dpr=1.5",
-      "https://www.looper.com/img/gallery/inosukes-powers-from-demon-slayer-explained/intro-1620465501.jpg",
+      "QmWxyHh6DZukfNjnurNJvT56TZVswAuwx3VnffKuy3kHPG",
+      "QmUeC5CUCY8Tk382wwSS5C4iQSNjwLW6ivXWsmsYjSadav",
+      "QmRKadv2aHZYsZbv2knteL1XPH7aT58n8r1JmEPyRkaMAx",
     ],
     [100, 150, 200],
     [100, 50, 40],
